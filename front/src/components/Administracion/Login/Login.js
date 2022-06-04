b9d5ab17a1ecf98ac7d4bb49cbae9ae216ace29f
Santi Comolli <santi.comolli@gmail.com>
2022-06-03 21:43:02 -0300

    Login y Registro editados y con algunas validaciones

--- front/src/components/Administracion/Login/Login.js
+++ front/src/components/Administracion/Login/Login.js
@@ -4,7 +4,10 @@ import axios from 'axios';
 import {useDispatch, useSelector} from 'react-redux'
 import Cookies from 'universal-cookie';
 import { getPais, getProvincia, getDepartamento, getLocalidad } from '../../../actions/actions';
+import diseño from "./login.css"
 const baseUrl = 'http://localhost:3001/usuario';
+
+
 
 function Login()
 {
@@ -58,7 +61,7 @@ function Login()
                 const cookies = new Cookies();
                 cookies.set('usuario',res.data[0].usuario,{path:'/'});
                 cookies.set('contrasena',res.data[0].contrasena,{path:'/'});
-                alert('Bienvenido');
+                alert("Bienvenido");
                 window.location.href = './admin';
             }
             else{
@@ -69,27 +72,64 @@ function Login()
 
 return(
 <div style={{width:'1000px', marginLeft:'450px', marginTop:'150px'}}>
-    <form class="row g-3">
-    <div class="col-md-4">
-        <label for="validationServer01" class="form-label">Usuario</label>
-        <input type="text" class="form-control" onChange={(e) => handleChange(e)} id="validationServer01" name='usuario' required></input>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12 col-md-4 offset-md-4">
+
+                <h2 id="titIniciar" class="text-center">Iniciar sesión</h2>
+
+                <form id="formulario " class="mt-5 d-grid gap-2">
+                    
+                    <label class="fw-bold form-label" for="validationServer01">Usuario</label>
+                    <input type="text" class="form-control" onChange={(e) => handleChange(e)} id="validationServer01" name='usuario' required maxLength={30}></input>
+                    <br></br>
+
+                    <label for="validationServer01" class="form-label fw-bold">Contraseña</label>
+                    <input name='contrasena' type="password" class="form-control" onChange={(e) => handleChange(e)} id="validationServer02" required maxLength={12}></input>
+
+                    <button class="btn btn-success btn-lg mt-4" type="submit" onClick={(e) => iniciarSesion(e)}>
+                        <Link class="nav-link" to='/admin'></Link>INGRESAR
+                    </button>
+                </form>
+
+                <hr class="text-white"></hr>
+
+                <b><p>
+                    No sos parte de nuestra familia?
+                    <Link class="nav-link text-decoration-none" to='/registro'>Registrate aqui</Link>
+                </p></b>
+
+            </div>
+        </div>
     </div>
 
-    <div class="col-md-4" style={{marginLeft:'335px'}}>
-        <label for="validationServer01" class="form-label">Contraseña</label>
-        <input name='contrasena' type="text" class="form-control" onChange={(e) => handleChange(e)} id="validationServer02"  required></input>
-    </div>
-    
-    <div class="col-12">
-        <button class="btn btn-primary" type="submit" onClick={(e) => iniciarSesion(e)}>
-            {/* <Link class="nav-link" to='/admin'></Link> */}
-            Ingresar
-        </button>
-    </div>
-    </form>
+
+    {/* <div class="d-flex">
+            <form class="row g-3">
+                <div class="col-md-4">
+                    <label for="validationServer01" class="form-label">Usuario</label>
+                    <input type="text" class="form-control" onChange={(e) => handleChange(e)} id="validationServer01" name='usuario' required></input>
+                </div>
+
+                <div class="col-md-4" style={{marginLeft:'335px'}}>
+                    <label for="validationServer01" class="form-label">Contraseña</label>
+                    <input name='contrasena' type="text" class="form-control" onChange={(e) => handleChange(e)} id="validationServer02"  required></input>
+                </div>
+                
+                <div class="col-12">
+                    <button class="btn btn-primary" type="submit" onClick={(e) => iniciarSesion(e)}>
+                        <Link class="nav-link" to='/admin'></Link> 
+                    </button>
+                </div>
+            </form>
+        </div> */}
+
+
 
     {/* ACA SE HACE EL REGISTRAR */}
-    <form class="row gy-2 gx-3 align-items-center" style={{marginTop:'150px'}} onSubmit={(e)=> post(e)}>
+
+
+    {/* <form class="row gy-2 gx-3 align-items-center" style={{marginTop:'150px'}} onSubmit={(e)=> post(e)}>
         <div class="col-auto">
             <label class="visually-hidden" for="autoSizingInput"></label>
             <input onChange={(e) => handleChange(e)} name='ape_nom' value={inputs.ape_nom} type="text" class="form-control" id="autoSizingInput" placeholder="Apellido y Nombre"></input>
@@ -154,8 +194,7 @@ return(
         <div>
             <button type="submit" class="btn btn-primary">Registrarse</button>
         </div>
-
-</form>
+    </form> */}
 </div>
 )}
 
---
+++ front/src/components/Administracion/Registro/Registro.js
@@ -0,0 +1,122 @@
+import { useEffect,useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {useDispatch, useSelector} from 'react-redux'
+import Cookies from 'universal-cookie';
+import { getPais, getProvincia, getDepartamento, getLocalidad } from '../../../actions/actions';
+import diseño from "../Login/login.css"
+
+const baseUrl = 'http://localhost:3001/usuario';
+
+
+
+function Registro()
+{
+    // const [inputs, setInputs] = useState({
+    //     usuario:'',
+    //     contrasena:''
+    // })
+
+    const dispatch = useDispatch();
+    var paises = useSelector(state => state.pais);
+    var departamentos = useSelector(state => state.departamento);
+    var localidades= useSelector(state => state.localidad);
+    var provincias = useSelector(state => state.provincia);
+
+    const [inputs, setInputs] = useState({
+        ape_nom:'',
+        domicilio:'',
+        email:'',
+        celular:'',
+        estado:'',
+        id_pais:'',
+        id_provincia:'',
+        id_departamento:'',
+        id_localidad:''
+    })
+
+    useEffect(() =>{
+        dispatch(getPais());
+        dispatch(getProvincia());
+        dispatch(getDepartamento());
+        dispatch(getLocalidad());
+    },[])
+
+    function handleChange(e) {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        })
+    } 
+
+    function post(e) {
+        e.preventDefault();
+        axios.post('http://localhost:3001/cliente', inputs)
+        alert(`${inputs.ape_nom} creado correctamente`)
+    }
+
+return(
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12 col-md-4 offset-md-4">
+
+                <h2 id="titIniciar" class="text-center mt-5 mb-5">Registrate!</h2>
+
+                <form class="row gy-2 gx-3 align-items-center"  onSubmit={(e)=> post(e)}>
+
+                    <label class="visually-hidden" for="autoSizingInput"></label>
+                    <input onChange={(e) => handleChange(e)} name='ape_nom' value={inputs.ape_nom} type="text" class="form-control mb-2" id="autoSizingInput" placeholder="Apellido y Nombre" required maxLength={40}></input>
+
+                    <label class="visually-hidden" for="autoSizingInput"></label>
+                    <input onChange={(e) => handleChange(e)} name='domicilio' value={inputs.domicilio} type="text" class="form-control  mb-2" id="autoSizingInput" placeholder="Domicilio" required maxLength={30}></input>
+
+                    <label class="visually-hidden" for="autoSizingInput"></label>
+                    <input onChange={(e) => handleChange(e)} name='email' value={inputs.email} type='email' class="form-control  mb-2" id="autoSizingInput" placeholder="Email" required></input>
+
+                    <label class="visually-hidden" for="autoSizingInput"></label>
+                    <input onChange={(e) => handleChange(e)} name='celular' value={inputs.celular} type="number" class="form-control  mb-2" id="autoSizingInput" placeholder="Celular" required maxLength={30}></input>
+
+                    <label class="visually-hidden" for="autoSizingInput"></label>
+                    <input onChange={(e) => handleChange(e)} name='estado' value={inputs.estado} type="text" class="form-control mb-2" id="autoSizingInput" placeholder="Estado" required maxLength={25}></input>
+
+                    <select class="form-select mb-2" id="inlineFormSelectPref" name='id_pais' required onChange={(e) => handleChange(e)}>
+                        <option selected>Pais...</option>
+                            {paises.map(pais => (
+                                <option key={pais.id} value={pais.id}>{pais.nombre}</option>
+                            ))}
+                    </select>
+
+                    <select class="form-select mb-2" id="inlineFormSelectPref" name='id_provincia' required onChange={(e) => handleChange(e)}>
+                        <option selected>Provincia...</option>
+                            {provincias.map(provincia => (
+                                <option key={provincia.id} value={provincia.id}>{provincia.nombre}</option>
+                            ))}
+                    </select>
+
+                    <select class="form-select mb-2" id="inlineFormSelectPref" name='id_departamento' required onChange={(e) => handleChange(e)}>
+                            <option selected>Departamento...</option>
+                            {departamentos.map(departamento => (
+                                <option key={departamento.id} value={departamento.id}>{departamento.nombre}</option>
+                                ))}
+                    </select>
+
+                    <select class="form-select mb-3" id="inlineFormSelectPref" name='id_departamento' required onChange={(e) => handleChange(e)}>
+                        <option selected>Localidad...</option>
+                            {localidades.map(localidad => (
+                                    <option key={localidad.id} value={localidad.id}>{localidad.nombre}</option>
+                                ))}
+                    </select>
+
+                    <hr></hr>
+
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-success btn-lg">Registrarse</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+)}
+
+export default Registro
