917d82dafc44b367e7601293031100a7a49ee1d1
Santi Comolli <santi.comolli@gmail.com>
2022-06-09 18:01:32 -0300

    tuki

--- front/src/components/Administracion/Admin.js
+++ front/src/components/Administracion/Admin.js
@@ -30,55 +30,55 @@ function Admin(){
                             <Link class="nav-link" to='/admin/articulo'>Articulos</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/tipoArticulo'>Tipo de Articulo</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/cliente'>Cliente</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/comprobante'>Comprobante</Link>
                         </li>  
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/configuracion'>Configuracion</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/localidad'>Localidad</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/departamento'>Departamento</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/provincia' >Provincia</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand">
                             <Link class="nav-link" to='/admin/pais'>Pais</Link>
                         </li>
                         <li class="nav-item navbar-brand">
-                        <Link class="nav-link disabled" >|</Link>
+                            <Link class="nav-link disabled" >|</Link>
                         </li>
                         <li class="nav-item navbar-brand " style={{marginTop:'10px'}}>
                             <button class="btn btn-warning" type="submit" onClick={(e) => cerrarSesion(e)}>Cerrar Sesion</button>
